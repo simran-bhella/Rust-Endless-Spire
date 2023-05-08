a4faab95a18c1e18a58622b474b8316aa7da30dd
DeeAhTee <deeahtee@live.com>
2023-05-08 15:37:04 -0700

    mroe crap

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use ggez::{
     event,
     glam::*,
-    graphics::{self},
+    graphics,
     Context, GameResult,
 };
 use std::{env, path};
@@ -12,6 +12,13 @@ struct MainState {
     image1: graphics::Image,
     image2: graphics::Image,
 }
+
+struct MainState {
+    window_settings: WindowSettings,
+    frames: f64,
+    angle: f32
+}
+
 
 impl MainState {
     /// Load images
@@ -137,14 +144,33 @@ impl event::EventHandler<ggez::GameError> for MainState {
 /*
 impl MainState {
     fn new() -> GameResult<MainState> {
-        let s = MainState { frames: 0.0 };
+        let s = MainState { frames: 0.0, angle: 0.0,
+            window_settings: WindowSettings {
+            toggle_fullscreen: true,
+            is_fullscreen: true,
+            resize_projection: false,
+        } };
         Ok(s)
     }
 }
 */
 
 impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        const DESIRED_FPS: u32 = 60;
+        while ctx.time.check_update_time(DESIRED_FPS) {
+            self.angle += 0.01;
+
+        if self.window_settings.toggle_fullscreen {
+            let fullscreen_type = if self.window_settings.is_fullscreen {
+                conf::FullscreenType::Desktop
+            } else {
+                conf::FullscreenType::Windowed
+            };
+            ctx.gfx.set_fullscreen(fullscreen_type)?;
+            self.window_settings.toggle_fullscreen = true;
+        }
+    }
         Ok(())
     }
 
@@ -180,13 +206,14 @@ impl event::EventHandler<ggez::GameError> for MainState {
         // }
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.9, 0.9, 0.3, 1.0]));
-
+   
         // Text is drawn from the top-left corner.
-        let offset = self.frames as f32 / 5.0;
+        let offset = self.frames as f32;
         let dest_point = ggez::glam::Vec2::new(offset, offset);
         canvas.draw(
             graphics::Text::new("Endless Spire")
-                .set_scale(48.),
+            // graphics::Text::new("press any button to start game")
+                .set_scale(69.),
             dest_point,
         );
 
